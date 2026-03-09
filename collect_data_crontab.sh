ebaf240425bbe73541079d543914c7a29434d264
Thomas-Nguyen12 <thomas.nguyen3612@gmail.com>
2026-03-09 20:37:03 +0000

    deleted the crontab.sh file

---
+++ collect_data_crontab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+
+# activating the anaconda environment
+
+conda activate weather_website
+
+
+cd /Users/thomasnguyen/weather_website/
+
+
+# running hte python script
+
+
+python3 collect_current_data.py
+
+
